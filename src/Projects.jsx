46c3c5566874175d90915f58fe92001def0025c9
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-22 20:45:15 +0700

    Setup projects.jsx

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useFetchProjects } from "./fetchProjects";
 
 const Projects = () => {
+  const { isLoading, projects } = useFetchProjects();
+
   return <div>Projects</div>;
 };
 
